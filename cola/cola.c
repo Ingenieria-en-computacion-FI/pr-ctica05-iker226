ae74a853654e7573ef888ac04ee131a04d980f62
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-03-30 16:35:51 -0600

    Update cola.c

--- cola/cola.c
+++ cola/cola.c
@@ -1,4 +1,8 @@
 #include "cola.h"
+#include "lista.h"
+#include "nodo.c"
+#include <stdio.h>
+#include <stdlib.h>
 
 Cola* cola_crear()
 {
@@ -7,42 +11,28 @@ Cola* cola_crear()
 
 int cola_vacia(Cola* cola)
 {
-    /*
-    TODO
-    usar vaciar_lista
-    */
+    return lista_vacia(cola);
 }
 
 void cola_enqueue(Cola* cola, int dato)
 {
-    /*
-    TODO
-    usar lista_insertar_tail
-    */
+    lista_insertar_tail(cola, dato);
 }
 
 int cola_dequeue(Cola* cola)
 {
-    /*
-    TODO
-    usar lista_eliminar_head
-    */
-    return -1;
+    return lista_eliminar_head(cola);
 }
 
 int cola_frente(Cola* cola)
 {
-    /*
-    TODO
-    regresar dato del head
-    */
-    return -1;
+    if(cola->head==NULL)
+     return -1;
+    
+    return cola->head->dato;
 }
 
 void cola_destruir(Cola* cola)
 {
-    /*
-    TODO
-    usar lista_destruir
-    */
+    lista_destruir(cola);
 }
