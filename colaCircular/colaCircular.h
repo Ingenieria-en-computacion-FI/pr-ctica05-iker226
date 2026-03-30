053ffe7a0e0ca3bc65247e3562fef9b1a94359d6
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-03-30 17:42:10 -0600

    Update colaCircular.h

--- colaCircular/colaCircular.h
+++ colaCircular/colaCircular.h
@@ -1,1 +1,25 @@
+#ifndef COLACIRCULAR_H
+#define COLACIRCULAR_H
 
+#include <stdbool.h>
+#include "lista.h"
+
+typedef Lista ColaCircular;
+
+ColaCircular* crear_colacircular();
+
+bool colacircular_empty(ColaCircular* cola);
+
+void vaciar_colacircular(ColaCircular* cola);
+
+void colacircular_enqueue(ColaCircular* cola, int dato);
+
+int colacircular_dequeue(ColaCircular* cola);
+
+int colacircular_frente(ColaCircular* cola);
+
+int colacircular_final(ColaCircular* cola);
+
+void destruir_colacircular(ColaCircular* cola);
+
+#endif
