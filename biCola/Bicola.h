3f7d2323434d86902e8ac2fc11bc03ec2af9f757
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-03-30 17:50:44 -0600

    Update Bicola.h

--- biCola/Bicola.h
+++ biCola/Bicola.h
@@ -1,1 +1,29 @@
+#ifndef BICOLA_H
+#define BICOLA_H
 
+#include <stdbool.h>
+#include "lista.h"
+
+typedef Lista Bicola;
+
+Bicola* crear_bicola();
+
+bool bicola_empty(Bicola* b);
+
+void bicola_push_frente(Bicola* b, int dato);
+
+void bicola_push_final(Bicola* b, int dato);
+
+int bicola_pop_frente(Bicola* b);
+
+int bicola_pop_final(Bicola* b);
+
+int bicola_frente(Bicola* b);
+
+int bicola_final(Bicola* b);
+
+void vaciar_bicola(Bicola* b);
+
+void destruir_bicola(Bicola* b);
+
+#endif
